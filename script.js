dd51679f3f642ec642025d7352e8c729a27cf831
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-17 18:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
-// function calculateMinCost() {
-// 	var arr = document.getElementById("rope-lengths").value.split(", ").map(function(item){
-// 		return parseInt(item,10);
-// 	});
-// 	let ans = 0;
-// 	arr.sort();
-// 	let i =0;
-// 	let j =1;
-
-// 	while(arr[j] != Number.MAX_VALUE){
-// 		console.log(arr[i],arr[j]);
-// 		ans = ans + arr[i]+arr[j];
-// 		arr[i] = arr[i]+arr[j];
-// 		arr[j] = Number.MAX_VALUE;
-// 		arr = sorted(arr.length , arr);
-// 	}
-// 	document.getElementById("result").innerText = ans;
-																		   
-  
-  
-// }  
-// function sorted(n,arr){
-// 	for(let i=0;i<n;i++){
-// 		for(let j=i+1;j<n;j++){
-// 			if(arr[i]>arr[j]){
-// 				let temp = arr[i];
-// 				arr[i] = arr[j];
-// 				arr[j] = temp;
-// 			}
-// 		}
-// 	}
-// 	return arr;
-// }
 function calculateMinCost() {
-    var arr = document.getElementById("rope-lengths").value.split(", ").map(function(item){
+    var input = document.getElementById("rope-lengths").value;
+    var ropeLengths = input.split(", ").map(function(item) {
         return parseInt(item, 10);
     });
-    let ans = 0;
-    arr.sort(function(a, b) {
+
+    ropeLengths.sort(function(a, b) {
         return a - b;
     });
-    let i = 0;
-    let j = 1;
 
-    while (arr[j] !== Number.MAX_VALUE) {
-        console.log(arr[i], arr[j]);
-        ans = ans + arr[i] + arr[j];
-        arr[i] = arr[i] + arr[j];
-        arr[j] = Number.MAX_VALUE;
-        arr = sorted(arr.length, arr);
-    }
-    document.getElementById("result").innerText = ans;
-}
+    var totalCost = 0;
 
-function sorted(n, arr) {
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
+    while (ropeLengths.length > 1) {
+        var combinedLength = ropeLengths.shift() + ropeLengths.shift();
+        totalCost += combinedLength;
+        ropeLengths.push(combinedLength);
+        ropeLengths.sort(function(a, b) {
+            return a - b;
+        });
     }
-    return arr;
+
+    document.getElementById("result").innerText = "Minimum cost: " + totalCost;
 }
 
